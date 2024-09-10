197807ef7f802d52a2e7a1f601d1170ac65a4e76
zeus <136583387+zeusssz@users.noreply.github.com>
2024-09-10 19:43:16 +0530

    Update setup.py

--- multicli/setup.py
+++ multicli/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='multicli',
-    version='1.0.2',
+    version='1.0.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
